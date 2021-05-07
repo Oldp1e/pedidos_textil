2e36d1f1b5057fe013bdd6f150e41d630a3eabcd
Samuel Pereira Lima Junior <samuellima06091999@gmail.com>
2021-05-07 10:15:15 -0300

    Banco de Dados do Sistema

--- dblogin.php
+++ dblogin.php
@@ -5,7 +5,7 @@ function OpenCon()
  $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "";
- $db = "sistema_textil";
+ $db = "BD_PEDIDOS";
 
 
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
